7bca01c72fc077aeebbb27fa3e3a9a004cca71a2
PavelZhulyakov <pavel.zhulyakov@cherishdev.com>
2021-09-17 15:07:51 +0300

    Onliner Mobilephones project

--- cypress/integration/ElectronikaOnliner.js
+++ cypress/integration/ElectronikaOnliner.js
@@ -3,24 +3,11 @@ import {Locators} from './Locators'
 import {Locators_main} from './Locators'
 import {urls_onliner} from './MainPageOnliner'
 
-/* describe('The Catalog main Page', () => {
-    const catalogPage = new CatalogMainPage()
 
-
-    it('Successfully loads Mobile page', () => {
-        catalogPage.visit_page('https://catalog.onliner.by/')
-        catalogPage.get_and_click(Locators.Electronika)
-        catalogPage.xpath_click(Locators.Mobilephones_acess)
-        //span[contains(text(),'Мобильные телефоны')]
-        catalogPage.xpath_click(Locators.Mobiles_only)
-
-
-        })
-    })*/
 describe('The Catalog main Page', () => {
 	Object.values(Locators).forEach((locators) => {
 		const catalogPage = new CatalogMainPage()
-		it('Successfully loads Mobile page', () => {
+		it('Successfully loads Mobile page' + locators[1], () => {
 			catalogPage.visit_page('https://catalog.onliner.by/')
 			// catalogPage.get_and_click(Locators.Electronika)
 			cy.contains('Электроника').click()
--- cypress/integration/Locators.js
+++ cypress/integration/Locators.js
@@ -1,23 +1,20 @@
-export const Locators =
-    {
-    	Mobiles_only: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[1]', 'https://catalog.onliner.by/mobile'],
-    	Headphones: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[2]', 'https://catalog.onliner.by/headphones'],
-    	Smartbracelets: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[3]', 'https://catalog.onliner.by/smartwatch'],
-    	Smartwatches: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[4]/span/span[3]', 'https://catalog.onliner.by/smartwatch'],
-    	Portable_charger: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[5]', 'https://catalog.onliner.by/portablecharger'],
-    	Memcards: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[6]', 'https://catalog.onliner.by/memcards'],
-    	Phonecases: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[7]', 'https://catalog.onliner.by/phonecase'],
-    	Protglass: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[8]', 'https://catalog.onliner.by/protectiveglass'],
-    	Phoneaccum: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[9]', 'https://catalog.onliner.by/phoneaccum'],
-    	Bluetooth: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[10]', 'https://catalog.onliner.by/bt'],
-    	Selfiestick: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[11]', 'https://catalog.onliner.by/selfiestick'],
-    	Phone_cable: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[12]', 'https://catalog.onliner.by/cable?cable_usage'],
-    	Carholder: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[13]', 'https://catalog.onliner.by/carholder'],
-    	Chargesmobile: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[14]', 'https://catalog.onliner.by/chargersmobile'],
-    	Wireless_charger: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[15]', 'https://catalog.onliner.by/chargersmobile'],
+export const Locators = {Mobiles_only: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[1]', 'https://catalog.onliner.by/mobile'],
+	Headphones: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[2]', 'https://catalog.onliner.by/headphones'],
+	Smartbracelets: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[3]', 'https://catalog.onliner.by/smartwatch'],
+	Smartwatches: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[4]/span/span[3]', 'https://catalog.onliner.by/smartwatch'],
+	Portable_charger: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[5]', 'https://catalog.onliner.by/portablecharger'],
+	Memcards: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[6]', 'https://catalog.onliner.by/memcards'],
+	Phonecases: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[7]', 'https://catalog.onliner.by/phonecase'],
+	Protglass: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[8]', 'https://catalog.onliner.by/protectiveglass'],
+	Phoneaccum: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[9]', 'https://catalog.onliner.by/phoneaccum'],
+	Bluetooth: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[10]', 'https://catalog.onliner.by/bt'],
+	Selfiestick: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[11]', 'https://catalog.onliner.by/selfiestick'],
+	Phone_cable: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[12]', 'https://catalog.onliner.by/cable?cable_usage'],
+	Carholder: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[13]', 'https://catalog.onliner.by/carholder'],
+	Chargesmobile: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[14]', 'https://catalog.onliner.by/chargersmobile'],
+	Wireless_charger: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[15]', 'https://catalog.onliner.by/chargersmobile'],
         Speackerdock: ['//*[@id="container"]/div/div/div/div/div[1]/div[3]/div/div[1]/div[1]/div/div[1]/div[2]/div/a[16]', 'https://catalog.onliner.by/speakerdock']
-
-    }
+}
 
 export const Locators_main =
     {Electronika: 'ul.catalog-navigation-classifier li[data-id="1"]',
